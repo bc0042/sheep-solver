8064f71246b2f468b4e768cc7e79994202f29977
bc <you@example.com>
2022-10-30 23:08:22 +0800

    protobufjs update

--- util/send.js
+++ util/send.js
@@ -1,16 +1,48 @@
 import helper from './helper.js'
 import axios from './ax.js'
+import path from 'path'
+import protobufjs from 'protobufjs'
 import { getSkinName } from '../data/skin.js'
 
-let send = 1
-let gameFile = process.argv[2]
-let gameType = process.argv[3] || 3
-let game = helper.load(gameFile)
+const send = 1
+const gameFile = process.argv[2]
+const gameType = process.argv[3] || 3
+const game = helper.load(gameFile)
+
+let stepInfoList = game.stepList.map(e => {
+    let timeTag = e > 0 ? parseInt(game.cards[e].type) : e
+    return {
+        chessIndex: e,
+        timeTag
+    }
+})
+
+
+// console.log(game.stepList.join(','))
+// console.log(stepInfoList)
+let matchPlayInfo = await matchPlayInfoToStr(stepInfoList, gameType)
+// console.log(matchPlayInfo)
+
+function matchPlayInfoToStr(stepInfoList, gameType) {
+    return new Promise((resolve) => {
+        protobufjs.load(path.join(process.cwd(), "data", "yang.proto"), (_, root) => {
+            const MatchPlayInfo = root.lookupType("yang.MatchPlayInfo");
+            const matchPlayInfo = {
+                gameType,
+                stepInfoList,
+            }
+            const buf = MatchPlayInfo.encode(matchPlayInfo).finish();
+            const b64 = Buffer.from(buf).toString("base64");
+
+            resolve(b64);
+        });
+    });
+}
+
 let params = {
     "MapSeed2": "",
-    // "MatchPlayInfo": "",
     "Version": "0.0.1",
-    "rank_role": 1,
+    "rank_role": 2,
     "rank_score": 1,
     "rank_state": 1,
     "rank_time": 333,
@@ -18,60 +50,15 @@ let params = {
 }
 params.MapSeed2 = game.matchInfo.data.map_seed_2
 
-
-function toHex(n) {
-    n = parseInt(n)
-    let h = n.toString(16)
-    if (n < 16) h = '0' + h
-    return h
-}
-
-let types = []
-let arr = game.stepList.map(e => {
-    if (e === -3) {
-        params.rank_role = 2
-        return '221608fdffffffffffffffff0110fdffffffffffffffff01'
-    } else if (e === -1) {
-        params.rank_role = 2
-        return '221608ffffffffffffffffff0110ffffffffffffffffff01'
-    } else if (e === -4) {
-        params.rank_role = 2
-        return '221608fcffffffffffffffff0110fcffffffffffffffff01'
-    }
-
-    let type = toHex(game.cards[e].type)
-    types.push(type)
-    let n = parseInt(e)
-    let h = toHex(n)
-    let line
-    if (n < 128) {
-        line = '220408' + h + '10' + type
-    } else {
-        line = '220508' + h + '0110' + type
-    }
-    return line
-})
-
-
-
 let url_over
 if (gameType == 3) {
-    arr.unshift('0803') //daily
-    let str = arr.join('')
-    console.log(str)
-    params.MatchPlayInfo = Buffer.from(str, 'hex').toString('base64')
+    params.MatchPlayInfo = matchPlayInfo
     url_over = 'https://cat-match.easygame2021.com/sheep/v1/game/game_over_ex?'
 } else {
-    arr.unshift('0804') //toppic
-    let str = arr.join('')
-    console.log(str)
-    params.play_info = Buffer.from(str, 'hex').toString('base64')
+    params.play_info = matchPlayInfo
     url_over = 'https://cat-match.easygame2021.com/sheep/v1/game/topic/game_over?'
 }
 
-// console.log(arr)
-// console.log(info)
-// console.log(types.join(','))
 
 console.log(params)
 console.log('gametype:', gameType)
