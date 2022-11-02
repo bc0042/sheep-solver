8b3af591d1b2500ca76342577034a7f12df302f5
bc <bc0042@126.com>
2022-11-02 17:00:17 +0800

    update

--- util/send.js
+++ util/send.js
@@ -6,15 +6,15 @@ import { getSkinName } from '../data/skins.js'
 
 const send = 1
 const gameFile = process.argv[2]
-const gameType = process.argv[3] == 3 ? 3 : 4
+const gameType = process.argv[3] ? 4 : 3
 const game = helper.load(gameFile)
 
 let stepInfoList = game.stepList.map(e => {
-    let timeTag = e > 0 ? parseInt(game.cards[e].type) : e
-    return {
-        chessIndex: e,
-        timeTag
-    }
+  let timeTag = e > 0 ? parseInt(game.cards[e].type) : e
+  return {
+    chessIndex: e,
+    timeTag
+  }
 })
 
 
@@ -24,49 +24,49 @@ let matchPlayInfo = await matchPlayInfoToStr(stepInfoList, gameType)
 // console.log(matchPlayInfo)
 
 function matchPlayInfoToStr(stepInfoList, gameType) {
-    return new Promise((resolve) => {
-        protobufjs.load(path.join(process.cwd(), "data", "yang.proto"), (_, root) => {
-            const MatchPlayInfo = root.lookupType("yang.MatchPlayInfo");
-            const matchPlayInfo = {
-                gameType,
-                stepInfoList,
-            }
-            const buf = MatchPlayInfo.encode(matchPlayInfo).finish();
-            const b64 = Buffer.from(buf).toString("base64");
+  return new Promise((resolve) => {
+    protobufjs.load(path.join(process.cwd(), "data", "yang.proto"), (_, root) => {
+      const MatchPlayInfo = root.lookupType("yang.MatchPlayInfo");
+      const matchPlayInfo = {
+        gameType,
+        stepInfoList,
+      }
+      const buf = MatchPlayInfo.encode(matchPlayInfo).finish();
+      const b64 = Buffer.from(buf).toString("base64");
 
-            resolve(b64);
-        });
+      resolve(b64);
     });
+  });
 }
 
 let params = {
-    "MapSeed2": "",
-    "Version": "0.0.1",
-    "rank_role": 2,
-    "rank_score": 1,
-    "rank_state": 1,
-    "rank_time": 333,
-    "skin": 1
+  "MapSeed2": "",
+  "Version": "0.0.1",
+  "rank_role": 2,
+  "rank_score": 1,
+  "rank_state": 1,
+  "rank_time": 333,
+  "skin": 1
 }
 params.MapSeed2 = game.matchInfo.data.map_seed_2
 
 let url_over
 if (gameType == 3) {
-    params.MatchPlayInfo = matchPlayInfo
-    url_over = 'https://cat-match.easygame2021.com/sheep/v1/game/game_over_ex?'
+  params.MatchPlayInfo = matchPlayInfo
+  url_over = 'https://cat-match.easygame2021.com/sheep/v1/game/game_over_ex?'
 } else {
-    params.play_info = matchPlayInfo
-    url_over = 'https://cat-match.easygame2021.com/sheep/v1/game/topic/game_over?'
+  params.play_info = matchPlayInfo
+  url_over = 'https://cat-match.easygame2021.com/sheep/v1/game/topic/game_over?'
 }
 
 
 console.log(params)
-console.log('gametype:', gameType)
+console.log('gametype:', gameType == 3 ? 'challenge' : 'topic')
 
 
 if (send) {
-    axios.post(url_over, params).then(res => {
-        console.log(res.data)
-        console.log('skin name:', getSkinName(res.data.data.skin_id))
-    })
+  axios.post(url_over, params).then(res => {
+    console.log(res.data)
+    console.log('skin name:', getSkinName(res.data.data.skin_id))
+  })
 }
